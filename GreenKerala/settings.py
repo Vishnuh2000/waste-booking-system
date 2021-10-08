545a5b9d394c0d7a9bf2c4036f993efafadd0357
Vishnu H <vishnuharidasqwe@gmail.com>
2021-10-08 19:24:42 +0530

    commit

--- GreenKerala/settings.py
+++ GreenKerala/settings.py
@@ -141,8 +141,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mainproject53@gmail.com'
-EMAIL_HOST_PASSWORD ='Project123'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD =''
 
 
 MEDIA_ROOT = 'uploads/'
